bc1ccc795fa86027c1130720d252f64452dbdd51
SuLoBoro <susan.longley@bglgroup.co.uk>
2020-10-23 16:30:21 +0100

    added question and answers

---
+++ src/quiz.js
@@ -0,0 +1,16 @@
+const catQuiz1 = {
+question: "What can you feed cats?",
+answer1: "cat food",
+answer2: "pizza",
+answer3: "coffee",
+answer4: "all of the above"
+
+};
+
+const catQuiz2 = {
+question: "Where do cats hang out?",
+answer1: "In the bar",
+answer2: "Sofa",
+answer3: "Bath Tubs",
+answer4: "Pool Hall"
+}
